0a19c56770cce3a86a50f4698ae374506cd89a77
Bochkovskyi <unundertale1@ukr.net>
2023-07-03 13:24:14 +0300

    refactor card

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -3,9 +3,12 @@ import React from "react";
 const Card = (props) => {
   const { className, children } = props;
   const CardTag = props.tagName ? props.tagName : "div";
-  const classes = className ? className + " card" : "card";
 
-  return <CardTag className={classes}>{children}</CardTag>;
+  return (
+    <CardTag className={className ? className + " card" : "card"}>
+      {children}
+    </CardTag>
+  );
 };
 
 export default Card;
