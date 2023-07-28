0cea2fe819bf33529e97a506bb270623d824c64b
Bochkovskyi <unundertale1@ukr.net>
2023-07-28 14:45:12 +0300

    integrate typescript

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("page-root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("page-root") as HTMLDivElement
+);
 
 root.render(<App />);
