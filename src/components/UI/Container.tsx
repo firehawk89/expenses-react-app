822c025b6a9e15969873203b900a631f832e0668
Bochkovskyi <unundertale1@ukr.net>
2023-07-29 20:45:41 +0300

    add container component

---
+++ src/components/UI/Container.tsx
@@ -0,0 +1,7 @@
+type ContainerProps = { children: React.ReactNode };
+
+const Container: React.FC<ContainerProps> = ({ children }) => {
+  return <div className="container">{children}</div>;
+};
+
+export default Container;
