666e25519c363e81ebb81b9dd4269dbc3051c72b
Bochkovskyi <unundertale1@ukr.net>
2023-07-29 21:33:07 +0300

    add Menu component and styles

---
+++ src/components/Layout/Menu.tsx
@@ -0,0 +1,23 @@
+const Menu: React.FC = () => {
+  return (
+    <ul className="menu">
+      <li>
+        <a className="menu__link" href="">
+          Home
+        </a>
+      </li>
+      <li>
+        <a className="menu__link" href="">
+          Log In
+        </a>
+      </li>
+      <li>
+        <a className="menu__link" href="">
+          Sign Up
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+export default Menu;
