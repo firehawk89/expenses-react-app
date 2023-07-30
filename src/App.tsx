60abb0e6a624f8371cf726bc0538a37759ecd5ef
Bochkovskyi <unundertale1@ukr.net>
2023-07-30 21:30:51 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { baseUrl } from "./utils/constants";
 import HomePage from "./pages/Home";
+import RegisterPage from "./pages/Register";
+import LoginPage from "./pages/Login";
+import RootLayout from "./pages/Root";
 
-const baseUrl = import.meta.env.BASE_URL;
-
-const router = createBrowserRouter([{ path: baseUrl, element: <HomePage /> }]);
+const router = createBrowserRouter([
+  {
+    path: baseUrl,
+    element: <RootLayout />,
+    children: [
+      { path: baseUrl, element: <HomePage /> },
+      { path: `${baseUrl}register`, element: <RegisterPage /> },
+      { path: `${baseUrl}login`, element: <LoginPage /> },
+    ],
+  },
+]);
 
 const App: React.FC = () => {
   return <RouterProvider router={router} />;
