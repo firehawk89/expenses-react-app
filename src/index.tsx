d0fd090dc9567448c826b4a089368d34d5d3a3ba
Bochkovskyi <unundertale1@ukr.net>
2023-07-31 14:23:26 +0300

    add styles folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 
-import "./index.scss";
+import "./styles/index.scss";
 import App from "./App";
 import ModalContextProvider from "./store/modal-context";
 
