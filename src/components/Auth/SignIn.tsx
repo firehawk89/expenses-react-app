ee5de320149b71db50dfc48a2a8cdddbf9b62b4a
Bochkovskyi <unundertale1@ukr.net>
2023-08-01 20:40:52 +0300

    refactor SignUp and SignIn

--- src/components/Auth/SignIn.tsx
+++ src/components/Auth/SignIn.tsx
@@ -1,13 +1,88 @@
+import { useState, useEffect } from "react";
+import useFormControl from "../../hooks/use-form-control";
+import User from "../../models/user-model";
 import AuthForm from "./AuthForm";
 import FormControl from "../UI/FormControl";
 import Button from "../UI/Button";
 import styles from "./AuthForm.module.scss";
 
+const checkInput = (value: string) => {
+  return value.trim().length !== 0;
+};
+
 const SignIn: React.FC = () => {
+  const {
+    value: usernameValue,
+    isValid: usernameIsValid,
+    inputChangeHandler: usernameChangeHandler,
+    inputBlurHandler: usernameBlurHandler,
+    inputClearHandler: usernameClearHandler,
+  } = useFormControl(checkInput);
+
+  const {
+    value: passwordValue,
+    isValid: passwordIsValid,
+    inputChangeHandler: passwordChangeHandler,
+    inputBlurHandler: passwordBlurHandler,
+    inputClearHandler: passwordClearHandler,
+  } = useFormControl(checkInput);
+
+  const [formIsValid, setFormIsValid] = useState<boolean | null>(false);
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      setFormIsValid(usernameIsValid && passwordIsValid);
+    }, 250);
+
+    return () => {
+      clearTimeout(identifier);
+    };
+  }, [usernameIsValid, passwordIsValid]);
+
+  const clearInputs = () => {
+    usernameClearHandler();
+    passwordClearHandler();
+  };
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (formIsValid) {
+      const userData: User = {
+        username: usernameValue,
+        password: passwordValue,
+      };
+
+      console.log(userData);
+
+      clearInputs();
+    } else if (!usernameIsValid) {
+      usernameBlurHandler();
+    } else {
+      passwordBlurHandler();
+    }
+  };
+
   return (
-    <AuthForm formType="login">
-      <FormControl label="Username" id="username" type="text" />
-      <FormControl label="Password" id="password" type="password" />
+    <AuthForm formType="login" onSubmit={submitHandler}>
+      <FormControl
+        label="Username"
+        id="username"
+        type="text"
+        value={usernameValue}
+        hasError={usernameIsValid !== null && !usernameIsValid}
+        onChange={usernameChangeHandler}
+        onBlur={usernameBlurHandler}
+      />
+      <FormControl
+        label="Password"
+        id="password"
+        type="password"
+        value={passwordValue}
+        hasError={passwordIsValid !== null && !passwordIsValid}
+        onChange={passwordChangeHandler}
+        onBlur={passwordBlurHandler}
+      />
       <Button className={styles["auth-form-btn"]} type="submit">
         Sign In
       </Button>
--- src/components/Auth/SignUp.tsx
+++ src/components/Auth/SignUp.tsx
@@ -1,14 +1,109 @@
+import { useState, useEffect } from "react";
+import useFormControl from "../../hooks/use-form-control";
+import User from "../../models/user-model";
 import AuthForm from "./AuthForm";
 import FormControl from "../UI/FormControl";
 import Button from "../UI/Button";
 import styles from "./AuthForm.module.scss";
 
+const checkInput = (value: string) => {
+  return value.trim().length !== 0;
+};
+
 const SignUp: React.FC = () => {
+  const {
+    value: emailValue,
+    isValid: emailIsValid,
+    inputChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    inputClearHandler: emailClearHandler,
+  } = useFormControl(checkInput);
+
+  const {
+    value: usernameValue,
+    isValid: usernameIsValid,
+    inputChangeHandler: usernameChangeHandler,
+    inputBlurHandler: usernameBlurHandler,
+    inputClearHandler: usernameClearHandler,
+  } = useFormControl(checkInput);
+
+  const {
+    value: passwordValue,
+    isValid: passwordIsValid,
+    inputChangeHandler: passwordChangeHandler,
+    inputBlurHandler: passwordBlurHandler,
+    inputClearHandler: passwordClearHandler,
+  } = useFormControl(checkInput);
+
+  const [formIsValid, setFormIsValid] = useState<boolean | null>(false);
+
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      setFormIsValid(emailIsValid && usernameIsValid && passwordIsValid);
+    }, 250);
+
+    return () => {
+      clearTimeout(identifier);
+    };
+  }, [emailIsValid, usernameIsValid, passwordIsValid]);
+
+  const clearInputs = () => {
+    emailClearHandler();
+    usernameClearHandler();
+    passwordClearHandler();
+  };
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (formIsValid) {
+      const userData: User = {
+        email: emailValue,
+        username: usernameValue,
+        password: passwordValue,
+      };
+
+      console.log(userData);
+
+      clearInputs();
+    } else if (!emailIsValid) {
+      emailBlurHandler();
+    } else if (!usernameIsValid) {
+      usernameBlurHandler();
+    } else {
+      passwordBlurHandler();
+    }
+  };
+
   return (
-    <AuthForm formType="register">
-      <FormControl label="Email" id="email" type="email" />
-      <FormControl label="Username" id="username" type="text" />
-      <FormControl label="Password" id="password" type="password" />
+    <AuthForm formType="register" onSubmit={submitHandler}>
+      <FormControl
+        label="Email"
+        id="email"
+        type="email"
+        value={emailValue}
+        hasError={emailIsValid !== null && !emailIsValid}
+        onChange={emailChangeHandler}
+        onBlur={emailBlurHandler}
+      />
+      <FormControl
+        label="Username"
+        id="username"
+        type="text"
+        value={usernameValue}
+        hasError={usernameIsValid !== null && !usernameIsValid}
+        onChange={usernameChangeHandler}
+        onBlur={usernameBlurHandler}
+      />
+      <FormControl
+        label="Password"
+        id="password"
+        type="password"
+        value={passwordValue}
+        hasError={passwordIsValid !== null && !passwordIsValid}
+        onChange={passwordChangeHandler}
+        onBlur={passwordBlurHandler}
+      />
       <Button className={styles["auth-form-btn"]} type="submit">
         Sign Up
       </Button>
