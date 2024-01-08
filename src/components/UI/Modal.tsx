3bed1dfc2bc72017722d82b1e6ff179c131b2242
Bochkovskyi <unundertale1@ukr.net>
2024-01-08 19:19:11 +0200

    refactor Modal component

--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -2,7 +2,7 @@ import { FC, useContext } from "react";
 import { ModalContext } from "../../store/modal-context";
 import Card from "./Card";
 import Button from "./Button";
-import { MODAL_BUTTON } from "../../types/enums/ModalButtons";
+import { MODAL_BUTTON } from "../../types/enums/ModalButton";
 
 type ModalProps = {
   title: string;
@@ -13,7 +13,7 @@ type ModalProps = {
 const Modal: FC<ModalProps> = ({ title, text, onConfirm }) => {
   const modalCtx = useContext(ModalContext);
 
-  const closeModalHandler = (event: React.SyntheticEvent<HTMLElement>) => {
+  const closeModal = (event: React.SyntheticEvent<HTMLElement>) => {
     if (
       (event.target as HTMLElement).classList.contains("modal") ||
       (event.target as HTMLButtonElement).id === MODAL_BUTTON.CANCEL
@@ -29,17 +29,13 @@ const Modal: FC<ModalProps> = ({ title, text, onConfirm }) => {
           ? "modal fixed z-[5] top-0 left-0 w-full h-full flex items-center justify-center opacity-100 visible bg-dark bg-opacity-60 transition-all"
           : "fixed -z-[1] top-0 left-0 w-full h-full flex items-center justify-center opacity-0 invisible bg-dark bg-opacity-60 transition-all"
       }
-      onClick={closeModalHandler}
+      onClick={closeModal}
     >
       <Card className="p-8 flex flex-col text-dark bg-light">
         <h2 className="mb-5 text-xl font-semibold text-center">{title}</h2>
         <p className="mb-8">{text}</p>
         <div className="flex justify-center gap-4">
-          <Button
-            id={MODAL_BUTTON.CANCEL}
-            type="button"
-            onClick={closeModalHandler}
-          >
+          <Button id={MODAL_BUTTON.CANCEL} type="button" onClick={closeModal}>
             Close
           </Button>
           <Button
