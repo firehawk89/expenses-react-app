f364b93cec8b9d13a721a07743502a4f380d2e81
Bochkovskyi <unundertale1@ukr.net>
2024-01-08 19:15:41 +0200

    refactor Expenses component

--- src/components/Expenses/Expenses.tsx
+++ src/components/Expenses/Expenses.tsx
@@ -7,25 +7,25 @@ import ExpensesChart from "./ExpensesChart";
 
 type ExpensesProps = {
   data: Expense[];
-  onDeleteItem: (id: string) => void;
   isLoading: boolean;
   error: string | null;
+  onDeleteItem: (id: string) => void;
 };
 
 const Expenses: FC<ExpensesProps> = ({
   data,
-  onDeleteItem,
   isLoading,
   error,
+  onDeleteItem,
 }) => {
-  const [year, setYear] = useState("none");
+  const [year, setYear] = useState<string>("");
 
   const onSelectedItemHandler = (selectedYear: string) => {
     setYear(selectedYear);
   };
 
   let filteredExpenses;
-  if (year !== "none") {
+  if (year) {
     filteredExpenses = data.filter((expense) => {
       return expense.date.getFullYear().toString() === year;
     });
@@ -38,9 +38,9 @@ const Expenses: FC<ExpensesProps> = ({
       <ExpensesFilter selected={year} onSelectedItem={onSelectedItemHandler} />
       <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList
+        expenses={filteredExpenses}
         isLoading={isLoading}
         error={error}
-        items={filteredExpenses}
         onDeleteItem={onDeleteItem}
       />
     </Card>
